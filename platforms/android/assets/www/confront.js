1a376a0eb0bb0c25a18b296a30432619f9afa033
edwincrug <edwincrug@gmail.com>
2016-02-19 13:39:30 -0600

    Se elimina proyecto PHONEGAP

--- platforms/android/assets/www/confront.js
+++
@@ -1,5 +0,0 @@
-var global_settings = {
-	urlCORS: 'http://192.168.20.9/ApiFlotilla'
-	//urlCORS: 'http://localhost:51694'
-	
-}
--- www/AngularJS/Perfil/perfilController.js
+++
@@ -1,4 +0,0 @@
-registrationModule.controller("perfilController", function ($scope, $route, $rootScope, loginRepository, alertFactory) {
-    
-	 //$scope.nombreCompleto = $rootScope.data.data.nombreCompleto;
-});
