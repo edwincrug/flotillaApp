1f973f1e091371bf4e9aceb449c34870e218f4d3
urielgangster <porkpie_mod@hotmail.com>
2016-03-02 20:31:11 -0600

    Consulta Expediente
    Se implementa la estructura para la búsqueda de expedientes

--- www/AngularJS/Expediente/consultaController.js
+++ www/AngularJS/Expediente/consultaController.js
@@ -1,26 +1,21 @@
 registrationModule.controller('consultaController', function ($scope, consultaRepository, $rootScope) {
-    
-    $scope.expedientes = consultaRepository.getExpedientes($rootScope.data.idUsuario);
 
-    $scope.$watch('expedientes', function() {
-        //alert('Cambio expedientes');
-    });
-
-    
-    
-    /*
-    $scope.Saludo = function () {
+    $scope.init = function () {
         alert('Invoca init');
-        $scope.expedientes = consultaRepository.getExpedientes($rootScope.data.idUsuario);
-        /* setInterval(function () {
-             $scope.expedientes = [];
-             $scope.showExpedientes();
-         }, 3000);
+        $scope.showExpedientes();
+        setInterval(function () {
+            $scope.showExpedientes();
+        }, 5000);
 
-    };*/
+    };
+
 
     $scope.showExpedientes = function () {
-        $scope.expedientes = consultaRepository.getExpedientes($rootScope.data.idUsuario);
+        consultaRepository.getExpedientes($rootScope.data.idUsuario).then(function successCallback(response) {
+            $scope.expedientes = response;
+        }, function errorCallback(response) {
+            alert("No recupera expedientes");
+        });
     }
 
     $scope.Detalle = function (expediente) {
--- www/AngularJS/Expediente/consultaRepository.js
+++ www/AngularJS/Expediente/consultaRepository.js
@@ -1,21 +1,34 @@
-registrationModule.factory('consultaRepository', function ($rootScope, $cordovaSQLite) {
-    var expedientes = [];
-    return {
-        getExpedientes: function (idUsuario) {
-            var query = "SELECT * FROM LicitacionUnidad WHERE usuarioAsignado = ?";
-            $cordovaSQLite.execute($rootScope.FlotillasDB, query, [idUsuario]).then(function (result) {
-                //alert('En el select');
-                if (result.rows.length > 0) {
-                    for (var i = 0; i < result.rows.length; i++) {
-                        expedientes.push(result.rows.item(i));
-                    }
-                } else {
-                    //alert("NO ROWS EXISTS");
-                }
-            }, function (error) {
-                console.log(error);
+registrationModule.factory('consultaRepository', function ($rootScope, $cordovaSQLite, DBA) {
+    var self = this;
+    var tablaRoles = [];
+    var descripcionRol = '';
+
+    self.getExpedientes = function (idUsuario) {
+        var parameters = [idUsuario];
+        return DBA.query("SELECT * FROM LicitacionUnidad WHERE usuarioAsignado = ?", parameters)
+            .then(function (result) {
+                return DBA.getAll(result);
             });
-            return expedientes;
-        }
     }
+    return self;
+
+    /*  var expedientes = [];
+      return {
+          getExpedientes: function (idUsuario) {
+              var query = "SELECT * FROM LicitacionUnidad WHERE usuarioAsignado = ?";
+              $cordovaSQLite.execute($rootScope.FlotillasDB, query, [idUsuario]).then(function (result) {
+                  //alert('En el select');
+                  if (result.rows.length > 0) {
+                      for (var i = 0; i < result.rows.length; i++) {
+                          expedientes.push(result.rows.item(i));
+                      }
+                  } else {
+                      //alert("NO ROWS EXISTS");
+                  }
+              }, function (error) {
+                  console.log(error);
+              });
+              return expedientes;
+          }
+      }*/
 });
